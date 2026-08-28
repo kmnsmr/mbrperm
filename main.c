65db0035eac3c1c3dbb88f2798a7a84fa80590e9
whirlt <whirlpoolgmd@gmail.com>
2026-08-28 23:27:45 +0200

    done with main

---
+++ main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "perm.h"
+#include "mbr.h"
+
+void
+fatal (const char* msg, const char* argv0)
+{
+	fprintf (stderr, "%s: %s", argv0, msg);
+	exit (EXIT_FAILURE);
+	__builtin_unreachable ();
+}
+	
+void
+usage (const char* argv0)
+{
+	printf ("Usage: %s <file> <permutation>", argv0);
+}
+
+int
+main (unsigned argc, char** argv)
+{
+	if (!argc)
+		{	
+			usage (argv[0]);
+			exit (EXIT_FAILURE);
+		}
+
+	if (argc == 1) // safe to say given permutation does nothing
+		exit (EXIT_SUCCESS);
+
+	unsigned* perm = malloc (sizeof (unsigned) * argc);
+
+	if (perm == NULL)
+		fatal ("main: malloc returned NULL pointer", argv[0]);
+
+	for (unsigned i = 0; i < argc; i++)
+		perm[i] = strtoul (argv[i]);
+
+	if (!valid_perm (perm))
+		fatal ("main: Invalid permutation", argv[0]);
+
+	FILE* file = fopen (argv[1], "rb");
+
+	if (file == NULL)
+		{
+fopen_fail:
+			switch (errno)
+				{
+				case ENOENT:
+					fatal ("main: No such file", argv[0]);
+					__attribute__ ((fallthrough)); // doesn't return
+				case EPERM:
+					fatal ("main: Operation not permitted", argv[0]);
+					__attribute__ ((fallthrough));
+				default:
+					fatal ("main: Could not open file for some reason", argv[0]);
+				}
+		}
+
+	struct mbr_t mbr;
+	
+	if (!read_mbr (&mbr, file))
+		{
+			fclose (file);
+			fatal ("main: Error reading MBR", argv[0]);
+		}
+
+	fclose (file);
+
+	struct mbr_t permuted;
+	copy_mbr (&permuted, &mbr);
+
+	unsigned preimage;
+	unsigned image;
+
+	for (unsigned* p = perm; cycle_perm (p, &image, &preimage);)
+		permuted.part[image] = mbr.part[preimage]; 
+
+	file = fopen (argv[1], "ab");
+
+	if (file == NULL)
+		goto fopen_fail;
+
+	write_mbr (&permuted, file);
+	fclose (file);
+
+	exit (EXIT_SUCCESS);
+}
