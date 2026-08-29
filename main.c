a9d1f695f5831d449873db5d7e42ba353b7d6174
whirlt <whirlpoolgmd@gmail.com>
2026-08-29 15:28:27 +0200

    added license and copyright notices

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * 
  * mbrperm is free software: you can redistribute it and/or modify it 
  * under the terms of the GNU General Public License v3 as published by the
- * Free Software Foundation version
+ * Free Software Foundation.
  *
  * mbrperm is distributed in the hope that it will be useful, 
  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
